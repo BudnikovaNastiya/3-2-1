e8fcbd58a6e1397be1035482865c321784cca7a3
Anastasiya Budnikova <budnikova.a.p@gmail.com>
2021-07-13 16:53:25 +0300

    initial commit

---
+++ src/main/java/domain/Post.java
@@ -0,0 +1,17 @@
+package domain;
+
+public class Post {
+    private int id;
+    private int photoId;
+    private geolocation;
+    private int date;
+    private int time;
+    private text;
+    private attachments;
+    private author;
+    private comments;
+    private likes;
+    private reposts;
+    private views;
+    private saveToFavorites;
+}
